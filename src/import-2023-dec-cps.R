a4de051ad3384b2f3aeca033d69d74c05f5b17d0
Lorae <10428186+lorae@users.noreply.github.com>
2025-10-06 15:43:18 -0400

    feat: import dec 2023 ASEC

---
+++ src/import-2023-dec-cps.R
@@ -0,0 +1,93 @@
+# import-2023-dec-cps.R
+#
+# This script processes raw IPUMS data and saves it in a DuckDB file.
+#
+# Input:
+# -  makes API call to IPUMS USA. Be sure to follow Part B of project set-up
+#    in README.md before running - this script reads an environment variable from 
+#    .Renviron
+#
+
+# ----- Step 0: Configuration ----- #
+library("dplyr")
+library("duckdb")
+library("ipumsr")
+library("glue")
+
+if (!file.exists(".Renviron")) {
+  stop(".Renviron file needed for this code to run. Please refer to Part B of the README file for configuration instructions.")
+} 
+
+# Read API key from project-local .Renviron
+readRenviron(".Renviron") # Force a re-read each run
+api_key <- Sys.getenv("IPUMS_API_KEY")
+
+if (api_key == "" || api_key == "your_ipums_api_key") {
+  stop(".Renviron file exists, but IPUMS API key has not been added. Please refer to Part B of the README file for configuration instructions.")
+}
+
+print(paste0("IPUMS API key: ", api_key))
+set_ipums_api_key(api_key)
+
+# Set the destination directories for the IPUMS data pull
+download_dir <- "data/ipums-microdata"
+db_dir <- "data/db"
+
+# ----- Step 1: Define, submit, and wait for data extract ----- #
+# Browse available samples and their aliases
+get_sample_info("cps") |> print(n=1000) 
+
+# Extact simple 2025 CPS ASEC: super recent county-level view of college students
+# on food stamps
+ipums_extract_2023_dec <- define_extract_micro(
+  description = "Food insecurity: 2023 Dec CPS",
+  collection = "cps",
+  samples = c(
+    "cps2023_12s" # Dec 2023 CPS
+  ),
+  variables = c(
+    "COUNTY", "AGE", "SCHLCOLL", 
+    "FSSTATUSD",
+    "FSSPDRESTC",
+    "FSTOTXPNC",
+    "FSBAL",
+    "FSSKIPYR",
+    "FSDAYF",
+    "FSFDBNK",
+    "FSFDBNKMO",
+    "FSEMRGFD",
+    "FSSHOPRESTLW",
+    "FSSHOPMKTLW",
+    "FSFOODS"
+  )
+)
+
+# Submit extract request
+submitted <- submit_extract(ipums_extract_2023_dec)
+
+# Poll until extract is ready
+wait_for_extract(submitted) 
+
+# ----- Step 2: Download and save extract ----- #
+
+# Once ready, download the extract ZIP
+download_extract(
+  submitted,
+  download_dir = download_dir,
+  overwrite = TRUE,
+  api_key = api_key
+)
+
+extract_num <- sprintf("%05d", submitted$number)
+
+ddi_path <- glue("{download_dir}/cps_{extract_num}.xml")
+dat_path <- glue("{download_dir}/cps_{extract_num}.dat.gz")
+
+# ----- Step 3: Save to DuckDB ----- #
+
+ddi <- read_ipums_ddi(ddi_path)
+ipums_tb <- read_ipums_micro(ddi, var_attrs = c()) 
+
+con <- dbConnect(duckdb::duckdb(), glue("{db_dir}/ipums.duckdb"))
+dbWriteTable(con, "ipums_2023_dec", ipums_tb, overwrite = TRUE)
+DBI::dbDisconnect(con)
